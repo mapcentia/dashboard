e82a45bcaf630999a51c838f55e88744d8fb94e9
Martin Høgh <mh@mapcentia.com>
2019-06-20 13:13:47 +0200

    Bug

--- app/components/ConfigurationListItem/index.js
+++ app/components/ConfigurationListItem/index.js
@@ -60,7 +60,7 @@ class PublishedConfigurationsPage extends React.Component {
                             style={{marginTop: `0px`}}/>
                     </div>
                     <div style={{textAlign: `right`}}>
-                        {this.props.gc2Configuration ? (<StyledExternalLink href={`${this.props.gc2Configuration.vidiUrl}/app/${databaseName}/?config=${url}`} target="_blank" style={{marginRight: `10px`}}>
+                        {this.props.gc2Configuration ? (<StyledExternalLink href={`${this.props.gc2Configuration.gc2Options.vidiUrl}/app/${databaseName}/?config=${url}`} target="_blank" style={{marginRight: `10px`}}>
                             <Button color="primary" variant="contained" size="small">
                                 <LaunchIcon/> Vidi
                             </Button>
